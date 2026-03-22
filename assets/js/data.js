8bf1c51e456a1ced9feca11169db00cb83bdfaf7
Malindu Rukshan <ruk.zh4n@gmail.com>
2026-03-22 23:17:12 +0530

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,65 +3,61 @@ export const socialLinks = [
     { 
         name: "GitHub", 
         desc: "Coding & Mods",    
-        url: "", 
+        url: "https://github.com/ruk-zh4n", 
         icon: "fab fa-github", 
         color: "text-white",
-        size: "small" // පොඩි කොටුවක්
+        size: "small" 
     },  
 
     { 
         name: "YouTube", 
         desc: "Tutorials & Explanations", 
-        url: "", 
+        url: "https://www.youtube.com/ruk-zh4n", 
         icon: "fab fa-youtube", 
         color: "text-red-600",
-        size: "wide" // දිග කොටුවක් (YouTube එකට විතරයි)
+        size: "wide" 
     },        
 
-    { 
-        name: "X (Twitter)", 
-        desc: "Updates",
-        url: "", 
-        icon: "fab fa-x-twitter", 
-        color: "text-white",
-        size: "small"
-    }
+   // { 
+     //   name: "X", 
+    //    desc: "Updates",
+   //     url: "", 
+   //     icon: "fab fa-x-twitter", 
+  //      color: "text-white",
+   //     size: "small"
+  //  }
 ];
 
 
 export const techStack = [
-    { name: "Kali Linux", icon: "fab fa-linux", color: "text-green-400" },
     { name: "Node.js", icon: "fab fa-js", color: "text-yellow-400" },
     { name: "Python", icon: "fab fa-python", color: "text-blue-400" },
-    { name: "Cyber Defense", icon: "fas fa-shield-alt", color: "text-white" },
-    { name: "Java", icon: "fab fa-java", color: "text-orange-400" },
     { name: "Databases", icon: "fas fa-database", color: "text-cyan-400" }
 ];
 
-
 export const projects = [
-    {
-        title: "",
-        category: "Research",
-        date: "Dec 2025",
-        desc: ".",
-        url: "",
-        color: "blue"
-    },
-    {
-        title: "",
-        category: "Open Source",
-        date: "Nov 2025",
-        desc: "",
-        url: "",
-        color: "read"
-    },
-    {
-        title: "",
-        category: "Open Source",
-        date: "2026",
-        desc: "",
-        url: "",
-        color: "orange"
-    }
+    //{
+        //title: "",
+        //category: "Research",
+       // date: "Dec 2025",
+       // desc: ".",
+       // url: "",
+      //  color: "blue"
+    //},
+   // {
+      //  title: "",
+     //   category: "Open Source",
+     //   date: "Nov 2025",
+     //   desc: "",
+     //   url: "",
+     //   color: "read"
+   // },
+  //  {
+   //     title: "",
+   //     category: "Open Source",
+  //      date: "2026",
+  //      desc: "",
+   //     url: "",
+  //      color: "orange"
+  //  }
 ];
